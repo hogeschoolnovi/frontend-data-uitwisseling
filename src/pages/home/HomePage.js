3d07ecb8b08def104086df666951f3a1a977d7f0
novaeeken <nova_eeken@hotmail.com>
2022-06-15 13:43:20 +0200

    Setup GET, POST and image upload

---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Studentendatabase NOVI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt earum eum excepturi facere ipsam maxime porro quibusdam reiciendis saepe similique!</p>
+    </div>
+  );
+}
+
+export default HomePage;
