677aef59a99c5a5b7c2025da92e27ed3ef2e507a
novaeeken <nova_eeken@hotmail.com>
2024-03-15 16:26:04 +0100

    Oude CRA bestanden weggehaald

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </Router>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Router>,
+)
